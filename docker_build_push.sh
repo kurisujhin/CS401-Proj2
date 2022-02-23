92ba1dec290841f3b9e9c146910fc51bde623569
kurisu <davidztl@hotmail.com>
2022-02-23 08:54:58 -0500

    v0.0.5

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,2 +1,2 @@
-docker build  -t kurisuandhutao/cs401_proj2:0.0.4 .
-docker push kurisuandhutao/cs401_proj2:0.0.4
+docker build  -t kurisuandhutao/cs401_proj2:0.0.$1 .
+docker push kurisuandhutao/cs401_proj2:0.0.$1
