df68b014697b2ff48c8b86cbd74ddacfdde93e29
kurisu <davidztl@hotmail.com>
2022-02-23 08:43:00 -0500

    v changed 0.0.4

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,2 +1,2 @@
-docker build  -t kurisuandhutao/cs401_proj2 .
-docker push kurisuandhutao/cs401_proj2
+docker build  -t kurisuandhutao/cs401_proj2:0.0.4 .
+docker push kurisuandhutao/cs401_proj2:0.0.4
--- predict.py
+++ predict.py
@@ -33,7 +33,7 @@ def create_app(test_config=None):
         data = json.loads(request.get_data())
         return jsonify(
             is_american=str(app.model.predict([data['text']])[0]),
-            version="kurisu_0.0.3",
+            version="kurisu_0.0.4",
             model_data="2022/02/20"
         )
 
